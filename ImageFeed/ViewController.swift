55a2b58e13b503459a9fc1d2957af85daec93921
ilman <kaimov-ilman@yandex.ru>
2026-01-03 01:25:07 +0300

    Start

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Ilman on 02.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
