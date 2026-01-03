cbe1ce459626a42443f52f0477302f0931c98fe1
ilman <kaimov-ilman@yandex.ru>
2026-01-04 00:01:46 +0300

    add costomVieCell

---
+++ ImageFeed/CostomVievCellTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CostomVievCellTableViewCell.swift
+//  ImageFeed
+//
+//  Created by Ilman on 03.01.2026.
+//
+
+import UIKit
+
+class CostomVievCellTableViewCell: UITableViewCell {
+
+    
+
+}
--- ImageFeed/ImageListViewController.swift
+++ ImageFeed/ImageListViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ImageListViewController: UIViewController {
 
+
+    @IBOutlet private var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.backgroundColor = .ypBlack
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ImageListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        0
+    }
+}
+
+extension ImageListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
