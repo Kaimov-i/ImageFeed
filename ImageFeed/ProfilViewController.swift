17b5f3bd812c76de78450b4735046a98e9ffbd91
ilman-makbook <kaimov-ilman@yandex.ru>
2026-01-24 03:33:17 +0300

    start sprint 9

---
+++ ImageFeed/ProfilViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilViewController.swift
+//  ImageFeed
+//
+//  Created by Ильман on 24.01.2026.
+//
+
+import UIKit
+
+class ProfilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
