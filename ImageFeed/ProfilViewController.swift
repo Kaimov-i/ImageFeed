553c133409b67ab15caec4319db43c5880403655
ilman-makbook <kaimov-ilman@yandex.ru>
2026-01-24 04:22:21 +0300

    Создание экрана профиля

--- ImageFeed/ProfilViewController.swift
+++ ImageFeed/ProfilViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ProfilViewController: UIViewController {
 
+    @IBOutlet weak var profilImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userInstagram: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
        
     }
-
+    
+    @IBAction func outButton(_ sender: UIButton) {
+    }
+    
 }
